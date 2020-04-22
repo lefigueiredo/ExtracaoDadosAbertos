8e610d745846f0c974ed29157a457d6eebc9150e
Letícia Figueiredo <leticia.ofigueiredo@gmail.com>
2020-04-21 21:28:54 -0300

    Codigo funcionando e simplificado verificando subpastas

--- outros.py
+++ outros.py
@@ -1,11 +1,13 @@
 from pyunpack import Archive
+import pathlib
 import os
 ###############################################
 ''' Fazendo a varredura dentro da pasta dos downloads tipo 01 '''
 
-path = ('C:\\Users\\Letícia\\Documents\\Desafios Python do Carlo\\teste_download\\')
+path = pathlib.Path('C:\\Users\\Letícia\\Documents\\Desafios Python do Carlo\\teste_download')
 
 extensoes = (".zip",".tar",".rar",".7z")
-for i in os.listdir(path):
-    if i.endswith(extensoes):
-        Archive(path+i).extractall(path)
+for i in extensoes:
+    for file in sorted(path.glob('**/*'+i)):
+        Archive(file).extractall(path)
+
